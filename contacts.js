48daa5dfbe9a6a548702748d1f98e870d9707a72
Dinara83 <mavleeva.dinara@gmail.com>
2023-03-24 23:04:37 +0300

    assembly

---
+++ contacts.js
@@ -0,0 +1,18 @@
+const contactsPath = ;
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  // ...твій код
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
